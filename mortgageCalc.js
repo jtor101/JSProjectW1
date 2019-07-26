2fe603df70dd3c9602ef36c7bd5743deb890b99e
John Torres <jtorres0802@gmail.com>
2019-07-26 14:36:53 -0400

    Mortgage Calculator functional, page created, semi-formatted.

---
+++ mortgageCalc.js
@@ -0,0 +1,41 @@
+// Annuity Calculator
+// by John Torres
+"use strict"
+
+let mortgagePrincipalJ, mtgIntRtJ, lengthOfLoanJ; // Inputs
+let mtgMonthlyJ, mtgTotalCostJ; // Outputs
+
+function getMortgageInputs() {
+    mortgagePrincipalJ = Number(document.getElementById("mtgPrin").value);
+    mtgIntRtJ = Number(document.getElementById("mtgInterestRate").value);
+    lengthOfLoanJ = Number(document.getElementById("loanLength").value);
+}
+
+function mortgageCalc() {
+    getMortgageInputs();
+    mtgMonthlyJ =
+        mortgagePrincipalJ * (mtgIntRtJ / 1200) /
+        (1 - Math.pow(1 + mtgIntRtJ / 1200, -12 * lengthOfLoanJ));
+
+    mtgTotalCostJ = mtgMonthlyJ * lengthOfLoanJ * 12;
+
+    document.getElementById("monthlyMortgage").value = mtgMonthlyJ.toFixed(2);
+    document.getElementById("totalCostMtg").value = mtgTotalCostJ.toFixed(2);
+} // Outputs come out, numbers not right.
+
+function init() {
+    const mortgageCalcBtn = document.getElementById("mtgCalc");
+    mortgageCalcBtn.onclick = mortgageCalc;
+}
+
+window.onload = init;
+
+// HTML IDs
+// mtgPrin: mortgagePrincipalJ
+// mtgInterestRate: mtgIntRtJ
+// loanLength: lengthOfLoanJ
+
+// monthlyMortgage: mtgMonthlyJ
+// totalCostMtg: mtgTotalCostJ
+
+// mtgCalc: mortgageCalcBtn (button)
